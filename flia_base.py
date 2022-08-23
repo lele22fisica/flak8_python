e6aba02ee8d213c7a6b54681d5896777de202ffe
Luiz Fernando de Souza <lele22fisica@gmail.com>
2022-08-23 17:54:02 -0300

    Importando abc

--- flia_base.py
+++ flia_base.py
@@ -1,4 +1,7 @@
-class FilaBase:
+import abc
+
+
+class FilaBase(metaclass=abc.ABCMeta):
     # Código é um atributo
     codigo: int = 0
     fila = []
@@ -10,3 +13,16 @@ class FilaBase:
             self.codigo = 0
         else:
             self.codigo += 1
+
+    @abc.abstractmethod
+    def gera_senha_atual(self):
+        ...
+
+    @abc.abstractmethod
+    def atualiza_fila(self):
+        ...
+
+    @abc.abstractmethod
+    def chama_cliente(self, caixa: int):
+        ...
+
